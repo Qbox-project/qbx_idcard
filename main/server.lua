222e979141959cd2f7b851b37a32a0311922f004
KostaZx <76644544+KostaZx@users.noreply.github.com>
2024-04-01 10:40:16 +0300

    remove inventory bridge
    remove inventory bridge

--- main/server.lua
+++ main/server.lua
@@ -1,5 +1,17 @@
 local sharedConfig = require 'config.shared'
 
+local ox_inventory = exports.ox_inventory
+
+function NewMetaDataLicense(src, itemName)
+    local newMetaDataItem = ox_inventory:Search(src, 1, itemName)
+    for _, v in pairs(newMetaDataItem) do
+        newMetaDataItem = v
+        break
+    end
+    newMetaDataItem.metadata.mugShot = lib.callback.await('um-idcard:client:callBack:getMugShot', src)
+    ox_inventory:SetMetadata(src, newMetaDataItem.slot, newMetaDataItem.metadata)
+end
+
 RegisterNetEvent('um-idcard:server:sendData', function(src,metadata)
     if metadata.mugShot ~= 'none' then
         lib.callback('um-idcard:client:callBack:getClosestPlayer', src, function(player)
